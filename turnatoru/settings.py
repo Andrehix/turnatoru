09d66cacc372f09bdfd50d1df93a9762f5fa0bf5
Iulian123 <iulianvl04@gmail.com>
2026-04-29 01:19:15 +0300

    test: make local test db self-contained

--- turnatoru/settings.py
+++ turnatoru/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+from importlib.util import find_spec
 from pathlib import Path
+import sys
 from dotenv import load_dotenv
 import os
 
@@ -56,8 +58,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+if find_spec('whitenoise') is not None:
+    MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
@@ -97,6 +101,14 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'test_db.sqlite3',
+        }
+    }
 
 
 # Password validation
